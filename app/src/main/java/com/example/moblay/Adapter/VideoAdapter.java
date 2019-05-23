b5c3cf33013c6c85333c4cd36ec80004d4b9a532
2056414 <2056414@student.uma.pt>
2019-05-23 18:31:56 +0100

    display all videos (thumbnails) from gallery

---
+++ app/src/main/java/com/example/moblay/Adapter/VideoAdapter.java
@@ -0,0 +1,70 @@
+package com.example.moblay.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.moblay.Model.VideoModel;
+import com.example.moblay.R;
+
+import java.util.ArrayList;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<VideoModel> arrayListVideos;
+    Activity activity;
+
+    public VideoAdapter(Context context, ArrayList<VideoModel> arrayListVideos, Activity activity) {
+        this.context = context;
+        this.arrayListVideos = arrayListVideos;
+        this.activity = activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_video, parent, false);
+        return new VideoAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Glide.with(context).load("file://" + arrayListVideos.get(position).getStr_thumb()).skipMemoryCache(false).into(holder._imageView);
+        holder._rl_selected.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        holder._rl_selected.setAlpha(0);
+
+        holder._rl_selected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "You clicked the video", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayListVideos.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView _imageView;
+        RelativeLayout _rl_selected;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            _imageView = (ImageView)itemView.findViewById(R.id.iv_image);
+            _rl_selected = (RelativeLayout)itemView.findViewById(R.id.rl_selected);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/moblay/MainActivity.java
+++ app/src/main/java/com/example/moblay/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.moblay;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.moblay.Adapter.VideoAdapter;
+import com.example.moblay.Model.VideoModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView _recyclerView;
+    RecyclerView.LayoutManager recycleviewLayoutManager;
+
+    ArrayList<VideoModel> arrayListVideos;
+
+    private static final int MY_PERMISSION_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            }
+        } else {
+            init(); //initialize all of the controls
+        }
+
+    }
+
+
+    private void init() {
+        _recyclerView = (RecyclerView) findViewById(R.id.recyclerViewVideo);
+        recycleviewLayoutManager = new GridLayoutManager(getApplicationContext(),2);
+        _recyclerView.setLayoutManager(recycleviewLayoutManager);
+        arrayListVideos = new ArrayList<>();
+        fetchVideosFromGallery();
+    }
+
+    private void fetchVideosFromGallery() {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, thum; //index data and thumbnail
+
+        String absolutePathImage = null;
+
+        uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI; //first, we initialize the URI
+
+        String[] projection = {MediaStore.MediaColumns.DATA,
+                MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+                MediaStore.Video.Media._ID,
+                MediaStore.Video.Thumbnails.DATA};
+
+        String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+
+        cursor = getApplicationContext().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        thum = cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
+
+        while(cursor.moveToNext()) {
+            absolutePathImage = cursor.getString(column_index_data);
+            VideoModel videoModel = new VideoModel();
+            videoModel.setBoolean_selected(false);
+            videoModel.setStr_path(absolutePathImage);
+            videoModel.setStr_thumb(cursor.getString(thum));
+
+            arrayListVideos.add(videoModel);
+        }
+
+        //call the adapter class and set it to recyclerview
+        VideoAdapter videoAdapter = new VideoAdapter(getApplicationContext(), arrayListVideos, this);
+        _recyclerView.setAdapter(videoAdapter);
+
     }
 }
